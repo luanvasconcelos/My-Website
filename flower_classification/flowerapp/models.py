5d594af731ec285103296fb6957b13f286789f76
Luan <luan-vasconcelos@hotmail.com>
2019-03-15 10:46:34 -0300

    Model class created and migrated to update database

--- flower_classification/flowerapp/models.py
+++ flower_classification/flowerapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Image(models.Model):
+    date_used = models.DateTimeField(default=timezone.now)
+    img = models.ImageField(upload_to='image_input')
--- flower_classification/flowerapp/views.py
+++ flower_classification/flowerapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'flowerapp/home.html', {'title': 'Luan Vasconcelos'})
+    data_dict = {
+        'title': 'Luan Vasconcelos',
+        # 'user_input': Image.Objects.last,
+    }
+    return render(request, 'flowerapp/home.html', data_dict)
 
 def prediction(request):
     return render(request, 'flowerapp/flower_prediction.html', {'title': 'Flower Prediction'})
